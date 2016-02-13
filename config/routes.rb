901f0a9b21e75cc5aaf2ebd75e9588367857f53d
aasare <aasarekode@gmail.com>
2016-02-13 18:38:54 +0200

    Initiate Message

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'tweets#index'
 
   resources :tweets
+  resources :messages, only: [:index]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160211012552) do
+ActiveRecord::Schema.define(version: 20160213163141) do
+
+  create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.text     "content",      limit: 65535, null: false
+    t.integer  "sender_id",                  null: false
+    t.integer  "recipient_id"
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+  end
 
   create_table "tweets", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.text     "content",    limit: 65535, null: false
